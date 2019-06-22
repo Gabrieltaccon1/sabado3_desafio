d8a60b0d03360a7bfbed03e99c6fc29c1ca8ade7
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-22 12:54:02 -0300

    sabado03

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercicio_sabado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            carro veiculo = new carro("Ferrari", "458" ,"Italia");
+
+
+            Console.WriteLine($"Marca:{veiculo.marca} \nModelo:{veiculo.modelo} \nPais de Origem:\n{veiculo.paisdeOrigem}\nQuantidade de Portas: {veiculo.qtdPortas()}\n" +
+                $"Flex ? :{veiculo.Flex()} \nCapacidade Tanque de Combustivel: {veiculo.capacidadeTanque()}  \nSeu gasto de Combustivel é: {veiculo.gastocombustivel()} Litros\nA autonomia por litro é de :{veiculo.autonomiaporLitro()}");
+        }
+    }
+}
---
+++ carro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicio_sabado
+{
+    class carro
+    {
+        public string marca, modelo, paisdeOrigem;
+       
+        public carro(string marca1,string modelo1,string paisdeOrigem1)
+         
+        {
+            marca = marca1;
+            modelo = modelo1;
+            paisdeOrigem = paisdeOrigem1;
+
+        }
+        public int qtdPortas() => 2;
+        public double capacidadeTanque() => 95;
+        public double autonomiaporLitro() => 5.5;
+        public bool Flex() => false;
+        public double distanciaPercorrida() => 100;      
+        public double gastocombustivel() => (capacidadeTanque() * autonomiaporLitro());
+
+        //  quantidadedeLitros = distancia * marcadodocarro;
+
+        // mediacosumo = distanciapercorrida / combustivelgasto;
+    }
+
+}
+
